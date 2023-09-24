17ba09b762336dbdfc5bf01ba390bca979fefe24
RaidenTrue <raidmgs@gmail.com>
2023-09-24 23:28:05 +0300

    Created AnimationBP for Character/Enemy - Using a Template ABP for Enemies.

--- Source/Aura/Private/Character/RpgBaseCharacter.cpp
+++ Source/Aura/Private/Character/RpgBaseCharacter.cpp
@@ -7,8 +7,11 @@
 ARpgBaseCharacter::ARpgBaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
@@ -18,17 +21,17 @@ void ARpgBaseCharacter::BeginPlay()
 	
 }
 
-// Called every frame
-void ARpgBaseCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void ARpgBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
+//// Called every frame
+//void ARpgBaseCharacter::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+//
+//// Called to bind functionality to input
+//void ARpgBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+//{
+//	Super::SetupPlayerInputComponent(PlayerInputComponent);
+//
+//}
 
--- Source/Aura/Public/Character/RpgBaseCharacter.h
+++ Source/Aura/Public/Character/RpgBaseCharacter.h
@@ -19,11 +19,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, Category = Combat)
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+//public:	
+//	// Called every frame
+//	virtual void Tick(float DeltaTime) override;
+//
+//	// Called to bind functionality to input
+//	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
