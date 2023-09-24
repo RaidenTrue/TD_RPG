cc67a8f453a027321f9c8857e1e3371ee948109e
RaidenTrue <raidmgs@gmail.com>
2023-09-24 03:10:30 +0300

    Added RPG & Enemy Character Classes

---
+++ Source/Aura/Public/Character/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Copyright by Savvas JRD - LTD Cyprus
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/RpgBaseCharacter.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyCharacter : public ARpgBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Aura/Public/Character/RpgCharacter.h
@@ -0,0 +1,17 @@
+// Copyright by Savvas JRD - LTD Cyprus
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/RpgBaseCharacter.h"
+#include "RpgCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ARpgCharacter : public ARpgBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
