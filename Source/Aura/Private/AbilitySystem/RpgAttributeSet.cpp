f9c349e712552501a77858893e9b6d7538172705
RaidenTrue <raidmgs@gmail.com>
2023-09-28 02:55:43 +0300

    Added & Initialized Attributes for Health & Mana

--- Source/Aura/Private/AbilitySystem/RpgAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/RpgAttributeSet.cpp
@@ -2,4 +2,46 @@
 
 
 #include "AbilitySystem/RpgAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+URpgAttributeSet::URpgAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(150.f);
+
+	InitMana(100.f);
+	InitMaxMana(200.f);
+}
+
+void URpgAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(URpgAttributeSet, Health, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(URpgAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(URpgAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(URpgAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void URpgAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URpgAttributeSet, Health, OldHealth);
+}
+
+void URpgAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URpgAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void URpgAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URpgAttributeSet, Mana, OldMana);
+}
+
+void URpgAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URpgAttributeSet, MaxMana, OldMaxMana);
+}
