c04c77446435693f1c2c9d88c2a5a07cb302fff3
RaidenTrue <raidmgs@gmail.com>
2023-09-30 03:31:54 +0300

    UI->HUD Displayed to Player using InitOverlay Function from RpgCharacter.cpp Class.

---
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright by Savvas JRD - LTD Cyprus
+
+
+#include "UI/WidgetController/OverlayWidgetController.h"
+
---
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright by Savvas JRD - LTD Cyprus
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/RpgWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UOverlayWidgetController : public URpgWidgetController
+{
+	GENERATED_BODY()
+	
+};
