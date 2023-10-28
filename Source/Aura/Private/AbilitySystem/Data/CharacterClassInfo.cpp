b5a781d441726ef1549c695421509493f9551ffe
RaidenTrue <raidmgs@gmail.com>
2023-10-28 03:17:53 +0300

    TMap CharacterClass - Implemented 3 Different Characters using enum Class.

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,5 @@
+// Copyright by Savvas JRD - LTD Cyprus
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,17 @@
+// Copyright by Savvas JRD - LTD Cyprus
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
