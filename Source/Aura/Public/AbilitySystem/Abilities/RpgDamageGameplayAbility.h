fbc9fe4802fcf4b2e1074f1f7e74d9ddcb5715f4
RaidenTrue <raidmgs@gmail.com>
2023-11-05 03:35:14 +0200

    RpgDamageGameplayAbility - DamageTypes & DamageEffect Class.

---
+++ Source/Aura/Public/AbilitySystem/Abilities/RpgDamageGameplayAbility.h
@@ -0,0 +1,17 @@
+// Copyright by Savvas JRD - LTD Cyprus
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/RpgGameplayAbility.h"
+#include "RpgDamageGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API URpgDamageGameplayAbility : public URpgGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
