843a0dcafd85132c3ee9918d2e4fba3c4083b56e
RaidenTrue <raidmgs@gmail.com>
2023-11-21 03:03:07 +0200

    Ability Class Summon & SpawnPoints Algorithm Part01

---
+++ Source/Aura/Public/AbilitySystem/Abilities/RpgSummonAbility.h
@@ -0,0 +1,36 @@
+// Copyright by Savvas JRD - LTD Cyprus
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/RpgGameplayAbility.h"
+#include "RpgSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API URpgSummonAbility : public URpgGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditAnywhere, Category = Summoning)
+	int32 NumMinions = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category = Summoning)
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category = Summoning)
+	float MinSpawnDistance = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = Summoning)
+	float MaxSpawnDistance = 250.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = Summoning)
+	float SpawnSpread = 90.f;
+};
